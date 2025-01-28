5df50cc11f50e61ddec2e188ea911bbf0af40698
FrancescoTullio <francescotullio1992@gmail.com>
2025-01-28 17:22:27 +0100

    feat:frontend

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -2,16 +2,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ReviewCard from "../components/ReviewCard";
+import ReviewForm from "../components/ReviewForm"
+
+const initialValues = {
+  name: "",
+  text: "",
+  vote: 0,
+};
 
 function FilmPage() {
   const { slug } = useParams();
 
   const [film, setFilm] = useState(null);
 
-  useEffect(() => {
-    axios.get(`http://localhost:3000/films/${slug}`).then((resp) => {
-      setFilm(resp.data.data);
+  
+
+  const [formData, setFormData] = useState(initialValues);
+
+  const getFilms = () => {
+    axios.get(`${backendUrl}/books/${slug}`).then((resp) => {
+      setBook(resp.data.data);
     });
+  };
+
+  useEffect(() => {
+    getFilms()
+  
   }, []);
 
   return (
@@ -33,6 +49,18 @@ function FilmPage() {
           <section>
             <div className="row row-cols-1 g-3">
               {film.reviews.map(curReview => <ReviewCard key={curReview.id} review={curReview} />)}
+            </div>
+          </section>
+          <section className="mt-5">
+            <div className="row justify-content-center">
+              <div className="col-8">
+                <h2 className="text-center">Invia una nuova recensione</h2>
+                <ReviewForm
+                  formData={formData}
+                  setFormData={setFormData}
+                  onSubmitFunction={storeReview}
+                />
+              </div>
             </div>
           </section>
         </>
